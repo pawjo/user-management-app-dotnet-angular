680a0e37ae1bebe0255e588127bf07a5ed33b703
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2022-02-27 02:06:08 +0100

    Added sql create database script

---
+++ UserManagementAPI/UserManagementAPI/SQL/CreateDatabase.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+	[Email] varchar(50) NOT NULL,
+	[Name] nvarchar(30) NOT NULL,
+	[Surname] nvarchar(30) NOT NULL,
+	[Age] int NOT NULL,
+	[ImageName] varchar(40) NULL
+)
