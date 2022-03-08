2a8c3d394bf3daa8a70bbe1b7afacadc5f4496a1
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2022-03-08 07:34:30 +0100

    Added user details component with routing

--- UserManagementWebApp/src/app/app-routing.module.ts
+++ UserManagementWebApp/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDetailsComponent } from './modules/user/user-details/user-details.component';
 import { UserListComponent } from './modules/user/user-list/user-list.component';
 
 
 const routes: Routes = [
-  { path: 'users', component: UserListComponent }
+  { path: 'users', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailsComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' }
 ];
 
 @NgModule({
