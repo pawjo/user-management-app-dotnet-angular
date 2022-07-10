cc67d788291eb2d1a504ba330f391344cdf459a3
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2022-07-11 00:50:27 +0200

    Changed subscribtion to async pipe in user details

--- UserManagementWebApp/src/app/modules/user/user-details/user-details.component.ts
+++ UserManagementWebApp/src/app/modules/user/user-details/user-details.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { UserDetails } from 'src/app/shared/models/user-details';
-import { UserImage } from 'src/app/shared/models/user-image';
-import { UserListItem } from 'src/app/shared/models/user-list-item';
 import { AppState } from 'src/app/store/app.state';
 import { loadUserDetails } from 'src/app/store/user.actions';
 import { selectUserDetails } from 'src/app/store/user.selectors';
@@ -17,25 +14,27 @@ export class UserDetailsComponent implements OnInit {
 
   userId: number = 0;
 
-  user?: UserDetails;
+  user$ = this.store.select(selectUserDetails);
 
   constructor(private store: Store<AppState>,
     private route: ActivatedRoute) { }
+  
 
   ngOnInit(): void {
     const param = this.route.snapshot.paramMap.get('userId');
     this.userId = parseInt(param!);
     this.store.dispatch(loadUserDetails({ userId: this.userId }));
-    this.store.select(selectUserDetails).subscribe(x => {
-      // if (!x.image) {
-      //   x.image = { name: '', url: this.userImageUrl, expiresOn: new Date() };
-      // }
-      this.user = x;
-    });
+    // this.store.select(selectUserDetails).subscribe(x => {
+    //   // if (!x.image) {
+    //   //   x.image = { name: '', url: this.userImageUrl, expiresOn: new Date() };
+    //   // }
+    //   this.user = x;
+    // });
   }
 
-  get userImageUrl(): string {
-    return this.user?.image ? this.user?.image.url : '';
-  }
+
+  // get userImageUrl(): string {
+  //   return this.user?.image ? this.user?.image.url : '';
+  // }
 
 }
